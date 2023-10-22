f0663d9345821fda6f048c1f987d7bb789558f80
LuciyVI <62550083+LuciyVI@users.noreply.github.com>
2023-10-22 21:50:14 +0300

    Update main.cpp
    add xml parser func from libxml2 and write value into config.xml

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 #include <cmath>
 #include <libxml/parser.h>
 #include <string>
+#include <libxml/tree.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+#include <stdlib.h>
 
+#define X ptr->x
+#define DEBUG
 using namespace std;
+
 
 
 struct data {
@@ -11,41 +18,69 @@ struct data {
 
 double x = 0;
 double a , b = 0;
+std::string string_func;
 
 };
 
 void calculating_func(struct data *ptr)
 {
     double func = (-0.5*cos(0.5*ptr->x)-0.5);
-    std::cout<<"Введите 'a' интервал на отрезке"<<std::endl;
-    std::cin>>ptr->a;
-    std::cout<<"Введите 'b' интервал на отрезке"<<std::endl;
-    std::cin>>ptr->b;
 };
 int open_and_change_config_file(struct data *ptr)
 {
-    std::string xml_file_name = "./config.xml";
-    std::cout << "Parsing " << xml_file_name << std::endl;
-    xmlDocPtr doc = xmlParseFile(xml_file_name.c_str());
-        if (doc == NULL) 
-            {
-                std::cerr << "Error parsing file " << std::endl;
-                return 1;
-            }
-    
-    std::cout<<doc->name<<std::endl;    
 
+
+xmlDocPtr doc = xmlParseFile("config.xml");
+xmlNodePtr cur = xmlDocGetRootElement(doc);
+xmlChar*  key;
+cur=cur->xmlChildrenNode;
+while(cur!=NULL){
+/* Найти подузел ключевого слова */
+if(!xmlStrcmp(cur->name,(const xmlChar*)"a")){
+    key=xmlNodeListGetString(doc,cur->xmlChildrenNode,1);
+    char* charString = (char*)key;
+    ptr->a=strtod(charString, NULL);
+}
+if(!xmlStrcmp(cur->name,(const xmlChar*)"b")){
+    key=xmlNodeListGetString(doc,cur->xmlChildrenNode,1);
+    char* charString = (char*)key;
+    ptr->b=strtod(charString, NULL);
+}
+if(!xmlStrcmp(cur->name,(const xmlChar*)"x")){
+    key=xmlNodeListGetString(doc,cur->xmlChildrenNode,1);
+    char* charString = (char*)key;
+    X=strtod(charString, NULL);
+}    
+if(!xmlStrcmp(cur->name,(const xmlChar*)"func")){
+    key=xmlNodeListGetString(doc,cur->xmlChildrenNode,1);
+    char* charString = (char*)key;
+    ptr->string_func = charString;
+
+    
+}    
+
+
+cur=cur->next;
+
+}
     return 0;
 };
-int main ()
+
+int main()
 {
-        struct data data;
-        struct data  *ptr_on_data = &data;
+
+    struct data data;
+    struct data  *ptr_on_data = &data;
     
         calculating_func(ptr_on_data);
-        std::cout<<data.a<<std::endl;
-        std::cout<<data.b<<std::endl;
         open_and_change_config_file(ptr_on_data);
+        
+#ifdef DEBUG        
+        std::cout<<data.a<<std::endl;
+        std::cout<<data.b<<std::endl; 
+        std::cout<<data.x<<std::endl; 
+        std::cout<<data.string_func<<std::endl; 
+#endif
     return 0;
 
 }

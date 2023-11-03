61207c00dce8cc99003b06364b924bbef155f9d9
LuciyVI <62550083+LuciyVI@users.noreply.github.com>
2023-11-03 09:19:02 +0300

    Update main.cpp
    "create func for paint_table"

--- main.cpp
+++ main.cpp
@@ -35,30 +35,43 @@ double cycle_calc_for_table(struct data *ptr)
         ptr->probability_P = 0.90;
         ptr->probability_Q = 0.005;
         int P = 0;
-        int q = 0;
-        for(P;ptr->probability_P<=0.99;)
+        for(P;ptr->probability_P<1;)
         {   
-        for(q; ptr->probability_Q<=0.1;) 
+        
+        for(int q = 0; ptr->probability_Q<0.105;) 
             {
             
                 ptr->Global_N_table[P][q] = calc_N(ptr);
-                std::cout<<std::fixed<<"Строки N внутри цикла в цикле = "<<ptr->Global_N_table[P][q]<<std::endl;
+                // std::cout<<std::fixed<<"Строки N внутри цикла в цикле = "<<ptr->Global_N_table[P][q]<<std::endl;
 
                 ptr->probability_Q+=0.005;
-                q++;
-                std::cout<<std::fixed<<"Строки P внутри цикла в цикле = "<<ptr->probability_P<<std::endl;
+                // std::cout<<std::fixed<<" итерация  q  = "<<q<<std::endl;
+            q++;
             }
             ptr->probability_P+=0.01;
-            std::cout<<std::fixed<<"Строки Q внутри цикле = "<<ptr->probability_Q<<std::endl;
+            // std::cout<<std::fixed<<"итерация  P = "<<P<<std::endl;
 
         P++;
         ptr->probability_Q = 0.005;
 
         }; 
-   
+        ptr->probability_P = 0.90;
+        ptr->probability_Q = 0.005;
 
         return 0;
-}
+};
+    int paint_table(struct data *ptr)
+{
+        std::cout<<"+-----+"<<"-----+"<<"-----+"<<"------+"<<"-----+"<<"------+"<<"------+"<<"------+"<<"------+"<<"------+"<<"------+"<<std::endl;
+        std::cout<<"| q/P |"<<"0.90"<<" |"<<"0.91"<<" | 0.92"<<" |"<<" 0.93"<<" |"<<"0.94"<<" | 0.95"<<" | 0.96"<<" | 0.97 " <<"| 0.98"<<" | 0.99 |" <<std::endl;
+        std::cout<<"+-----+"<<"-----+"<<"-----+"<<"------+"<<"------+"<<"-----+"<<"------+"<<"------+"<<"------+"<<"------+"<<"------+"<<std::endl;
+        for(int i=0;i<=10;i++)
+        {
+            
+        }
+        return 0;
+};
+
 int main()
 {
 
@@ -82,12 +95,14 @@ int main()
     // calc_extremum(ptr_on_data);
     cycle_calc_for_table(ptr_on_data);
     int i,j;
-
-    for(i=0;i<10;i++){
-        for(j=0;j<20;j++){
-            std::cout<<std::fixed<<data.Global_N_table[6][15]<<std::endl;
-        }
-    }
+    
+    // for(i=0;i<10;i++){
+    
+    //         for(j=0;j<20;j++){
+    //         std::cout<<std::fixed<<data.Global_N_table[i][j]<<std::endl;
+    //     }
+    // }
+    paint_table(ptr_on_data);
     return 0;
 
 }

1c463434ce3706229107ffa0f6e9bdc0b6fa6a21
LuciyVI <62550083+LuciyVI@users.noreply.github.com>
2023-11-07 20:56:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include <random>
 #include "func.hpp"
 #include <boost/format.hpp>
-
+#include <iomanip>
 
 #define DEBUG
 // using namespace std;
@@ -48,7 +48,7 @@ double cycle_calc_for_table(struct data *ptr)
                 // std::cout<<std::fixed<<"Строки N внутри цикла в цикле = "<<ptr->Global_N_table[P][q]<<std::endl;
 
                 ptr->probability_Q+=0.005;
-                // std::cout<<std::fixed<<" итерация  q  = "<<q<<std::endl;
+ +               // std::cout<<std::fixed<<" итерация  q  = "<<q<<std::endl;
             q++;
             }
             ptr->probability_P+=0.01;
@@ -91,7 +91,32 @@ double cycle_calc_for_table(struct data *ptr)
 };
 
 
+   int paint_table_func_x(struct data *ptr)
+{       
 
+        std::string str1 = "\n +--------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+--------------+ \n";
+        std::string str2 = "|     q/P      |     0.90     |      0.91    |     0.92     |      0.93    |     0.94     |      0.95    |      0.96    |    0.97      |     0.98     |     0.99     | ";
+ 
+        std::cout << boost::format("%1$10d %2$10d %3$10d")% str1 % str2 % str1  << std::endl;
+        
+        
+        int j=0;
+
+
+        for(int i=0;i<20;)
+        {
+
+            
+            std::cout<<std::setprecision(5)<<boost::format("%1$13d %2$13d %3$13d %4$18d %5$13d %6$15d %7$13d %8$15d %9$15d %10$18d %11$12d %12$13d " ) % str1 % ptr->probability_Q % ptr->Global_Fx_table[j][i] % ptr->Global_Fx_table[j+1][i] % ptr->Global_Fx_table[j+2][i] % ptr->Global_Fx_table[j+3][i] % ptr->Global_Fx_table[j+4][i] % ptr->Global_Fx_table[j+5][i]% ptr->Global_Fx_table[j+6][i] % ptr->Global_Fx_table[j+7][i]%  ptr->Global_Fx_table[j+8][i]% ptr->Global_Fx_table[j+9][i] << std::endl;
+            
+            
+            ptr->probability_Q+=0.005;
+            
+        i++;
+        }
+
+            return 0;
+};
 
 int main()
 {
@@ -111,14 +136,16 @@ int main()
         std::cout<<"Q - "<<data.probability_interval<<"\n"<<std::endl;
 #endif
     
-    parse_string_for_func(ptr_on_data);
     parse_string_for_func_multi(ptr_on_data);
     // calc_extremum(ptr_on_data);
     cycle_calc_for_table(ptr_on_data);
     
     
+    calc_func_y(ptr_on_data);
     paint_table(ptr_on_data);
+    std::cout<<"\n ===================================================================================================================================================== \n"<<std::endl;
+    paint_table_func_x(ptr_on_data);
+    std::cout<<data.string_func_multi<<std::endl;
     return 0;
-
 }
     
